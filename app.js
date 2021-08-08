df0d286fdd7ddde408353b98701ff56620584e1c
LaChiThanh <thanhlahh999@gmail.com>
2021-08-08 17:40:42 +0700

    update code

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(function (err, req, res, next) {
     })
 })
 
-const PORT=3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log(`run server http://localhost:${PORT}`);
